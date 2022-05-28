6d6fd08189c9a2200ab72141c4ea699098d7b455
amitbar2001 <amitbar2001@gmail.com>
2022-05-28 15:02:51 +0300

    update gitignore

--- server/dist/socket/middlewares.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userMiddleware = void 0;
-const userMiddleware = (socket, next) => {
-};
-exports.userMiddleware = userMiddleware;
